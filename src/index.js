a22935b5feed1084956cb60bd2e8eb1008fa5460
Matteo Piccini <gnappo.1@live.it>
2023-09-19 08:21:20 -0700

    Final code for review

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //! Global vars
+let currentFilm;
 
 const nav = document.querySelector("#movie-list")
 const movieImgDetail = document.querySelector("#detail-image")
@@ -7,9 +8,19 @@ const movieReleaseYear = document.querySelector("#year-released")
 const movieDesc = document.querySelector("#description")
 const movieWatched = document.querySelector("#watched")
 const movieBloodAmount = document.querySelector("#amount")
+const bloodForm = document.querySelector("#blood-form")
 
-let currentFilm;
 //! Helper functions
+const displayMoviePoster = filmObj => {
+    let image = document.createElement("img")
+    image.src = filmObj.image
+    image.alt = filmObj.title
+
+    //! Attach the listener here deliverable 3
+    image.addEventListener("click", () => displayMovieDetails(filmObj))
+    nav.append(image)
+}
+
 const displayMovieDetails = movie => {
     currentFilm = movie
     movieImgDetail.src = movie.image
@@ -18,30 +29,50 @@ const displayMovieDetails = movie => {
     movieDesc.textContent = movie.description
     movieWatched.textContent = movie.watched ? "Watched" : "Unwatched"
     movieBloodAmount.textContent = movie["blood_amount"]
-    movieWatched.addEventListener("click", (e) => {
-        //! Change the button text
-        const newText = e.target.textContent === "Unwatched" ? "Watched" : "Unwatched"
-        e.target.textContent = newText
-        //! Change the object itself TEMPORARILY
-        movie.watched = !movie.watched
-    })
+}
+
+const toggleWatchedProperty = (e) => {
+    //! Change the button text
+    const newText = e.target.textContent === "Unwatched" ? "Watched" : "Unwatched"
+    e.target.textContent = newText
+    //! Change the object itself TEMPORARILY
+    currentFilm.watched = !currentFilm.watched
+}
+
+const toggleBloodCount = e => {
+    //! NO PAGE REFRESHES EVER
+    e.preventDefault()
+    //! Figure out the number typed
+    const droplets = e.target['blood-amount'].value
+    //! Modify the object's property
+    currentFilm["blood_amount"] += parseInt(droplets) || 0
+    //! Update the page
+    movieBloodAmount.textContent = currentFilm["blood_amount"]
+    //! Reset the form
+    e.target.reset()
 }
 
 //! Execute code
 
+//! Get all movies and display only the first one (Deliverable 1 & 2)
 fetch("http://localhost:3000/movies")
 .then(response => response.json())
 .then(movies => {
     //! TAKE CARE OF FIRST MOVIE
     displayMovieDetails(movies[0])
     //! LOAD EVERY IMAGE INTO NAV
-    movies.forEach(filmObj => {
-        let image = document.createElement("img")
-        image.src = filmObj.image
-        image.alt = filmObj.title
-
-        //! Attach the listener here
-        image.addEventListener("click", () => displayMovieDetails(filmObj))
-        nav.append(image)
-    })
+    movies.forEach(displayMoviePoster)
 })
+
+//! Deliverable 4
+movieWatched.addEventListener("click", toggleWatchedProperty)
+
+//! Deliverable 5
+bloodForm.addEventListener("submit", toggleBloodCount)
+
+
+// TODO REFLECTIONS
+//! The exercise could be improved firing appropriate PATCH requests
+//! Any time the "watched" and "blood_amount" properties need to change
+//! And then updating the DOM accordingly
+//! This way there would be no need for a global variable to preserve memory of the current film.
